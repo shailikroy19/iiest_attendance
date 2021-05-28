b17e988e40f0c588a0213f5ba0a91a63e4485b4d
Pranab1109 <pranabsaha1130@gmail.com>
2021-05-29 00:04:14 +0530

    deleted faltu comment

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //aniket
       debugShowCheckedModeBanner: false,
       title: 'IIEST Attendance Portal',
       theme: ThemeData(
