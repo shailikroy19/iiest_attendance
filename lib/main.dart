a6fef02a3595ba5a7317642c2fa4b2c20bfe8e23
Aniket Majhi <aniketmajhi2001@gmail.com>
2021-05-28 23:55:10 +0530

    Faltu

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //aniket
       debugShowCheckedModeBanner: false,
       title: 'IIEST Attendance Portal',
       theme: ThemeData(
